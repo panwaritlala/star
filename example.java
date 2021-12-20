e9252d1677434034b29e371a83948a25ffcde3ae
Your Name <you@example.com>
2021-12-20 18:07:35 +0700

    first commit

---
+++ example.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class example.java {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int[] number = new int[3];
+        // System.out.println(number.length);
+
+        for (int i = 0; i < number.length; i++) {
+            System.out.print("Enter Number : ");
+            number[i] = kb.nextInt();
+        }
+        System.out.print("{");
+        int sum = 0;
+        for (int i : number) {
+            if (sum<=i) {
+                System.out.print(i+",");
+            } else {
+                System.out.print(i);
+                
+            }
+            sum = sum + i;           
+        }
+        System.out.println("}");
+        System.out.println(sum);
+        kb.close();
+    }
+}
+
+
+
